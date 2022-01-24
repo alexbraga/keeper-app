a32a7099c283562ad22904ed6c9bd071d9da4660
Alex Braga <contato@alexbraga.com.br>
2022-01-24 04:23:56 -0300

    Updates

---
+++ precache-manifest.cb53999ded53905d55c452853ad903ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d367bc388be5a4434e981ecb1ac355b5",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "75e26f885a90ef422e23",
+    "url": "/keeper-app/static/js/2.7a0aedac.chunk.js"
+  },
+  {
+    "revision": "4844a3cf5a99d1261bd981ecae8befeb",
+    "url": "/keeper-app/static/js/2.7a0aedac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff13e55d11832b786be4",
+    "url": "/keeper-app/static/js/main.5df0b891.chunk.js"
+  },
+  {
+    "revision": "3f7ec364bc84954cf321",
+    "url": "/keeper-app/static/js/runtime-main.afaa4161.js"
+  }
+]);
